f486b65c48e085e7f6e3ffd0cd0c51056e455b6b
nr-baek <nara130825@gmail.com>
2022-12-26 21:42:35 +0900

    초기 테스트

---
+++ pages/sub/[id].tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function About() {
+  const router = useRouter();
+  const id = Number(router.query.id);
+  return (
+    <>
+      <h1>/pages/sub/[id].tsx</h1>
+      <p>Parameter id: {id}</p>
+      <Link href="/">/pages/index.tsx</Link>
+    </>
+  );
+}
---
+++ pages/sub/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function SubIndex() {
+  return (
+    <>
+      <h1>/pages/sub/index.tsx</h1>
+      <Link href="/">/pages/index.tsx</Link>
+    </>
+  );
+}
