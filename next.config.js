3b6fd7c8cfd28a0b0c12fa6c1227da74e6bdfdcd
nr-baek <nara130825@gmail.com>
2022-12-27 21:52:35 +0900

    prefix 경로 수정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const repository = "https://nr-baek.github.io/Calculator";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  assetPrefix: !debug ? `/Calculator/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
